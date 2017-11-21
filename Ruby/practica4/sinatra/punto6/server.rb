5aafe995fc1da090f68dafd1160512184a027426
Pedro Brost <pedro.brost97@gmail.com>
2017-11-20 23:34:26 -0300

    p4-p6-ruby

---
+++ Ruby/practica4/sinatra/punto6/server.rb
@@ -0,0 +1,36 @@
+require 'bundler'
+require_relative 'number_to_x.rb'
+Bundler.require
+
+use NumberToX
+
+get '/' do
+  s = "Las rutas disponibles son: <br>"
+  Sinatra::Application.routes['GET'].reduce(s) do |res, r|
+    res << r.verb + " " + r.path + "<br>"
+  end
+end
+
+get '/mcm/:a/:b' do |a, b|
+  a.to_i.lcm(b.to_i).to_s
+end
+
+get '/mcd/:a/:b' do |a, b|
+  a.to_i.gcd(b.to_i).to_s
+end
+
+get '/sum/*' do |nums|
+  nums.split('/').map(&:to_i).sum.to_s
+end
+
+get '/even/*' do |nums|
+  nums.split('/').map(&:to_i).select(&:even?).size.to_s
+end
+
+post '/random' do
+  rand.to_s
+end
+
+post '/random/:lower/:upper' do |lower, upper|
+  rand(lower.to_i..upper.to_i).to_s
+end
